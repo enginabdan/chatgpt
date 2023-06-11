c1b7199ae4e780b92c5270d8308a1758b0e7a406
enginabdan <engin.abdan@gmail.com>
2023-06-12 02:08:39 +0300

    a

--- chatgpt/android/app/src/main/kotlin/com/jagoteori/chatgpt/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.jagoteori.chatgpt
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- chatgpt/lib/viewmodel/get_question_answer_event.dart
+++
@@ -1,3 +0,0 @@
-part of 'get_question_answer_bloc.dart';
-
-abstract class GetQuestionAnswerEvent {}
